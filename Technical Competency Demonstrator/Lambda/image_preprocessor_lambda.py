978d49585b114a473043405c90569e5313073a76
Bryce Sandilands <bryce.sandilands@gmail.com>
2022-06-04 15:09:27 -0700

    Preprocessing lambda refactor and unit tests

--- Technical Competency Demonstrator/Lambda/image_preprocessor_lambda.py
+++ Technical Competency Demonstrator/Lambda/image_preprocessor_lambda.py
@@ -1,32 +1,31 @@
 import boto3
 import uuid
-import cv2 as cv
-
-s3_client = boto3.client("s3")
-S3_BUCKET_NAME = "sagemaker-training-csu303"
-S3_DEST_BUCKET_NAME = "preprocessed-images-csu303"
-S3_PREFIX = "sm"
 
 
-def get_file_from_bucket(source_bucket, key, img_file):
-    s3_client.download_fileobj(source_bucket, key, img_file)
+def get_bucket_names(source, destination):
+    source = source or "sagemaker-training-csu303"
+    destination = destination or "preprocessed-images-csu303"
+    return source, destination
+
+
+def get_file_from_bucket(client, source_bucket, key, img_file):
+    client.download_fileobj(source_bucket, key, img_file)
 
 
 def process_image(img_path):
-    img = cv.imread(img_path)
-    edges = cv.Canny(img, 65, 150)
-    cv.imwrite(img_path, edges)
+    pass
 
 
-def add_file_to_bucket(img_path, upload_key):
-    s3_client.upload_file(img_path, S3_DEST_BUCKET_NAME, upload_key)
+def add_file_to_bucket(client, img_path, dest_bucket_name, upload_key):
+    client.upload_file(img_path, dest_bucket_name, upload_key)
 
 
-def remove_file_to_bucket(source_bucket, key):
-    s3_client.delete_object(source_bucket, key)
+def remove_file_from_bucket(client, source_bucket, key):
+    client.delete_object(Bucket=source_bucket, Key=key)
 
 
 def lambda_handler(event, context):
+    s3_client = boto3.client("s3")
     # getting bucket and object key from event object
     source_bucket = event["Records"][0]["s3"]["bucket"]["name"]
     key = event["Records"][0]["s3"]["object"]["key"]
@@ -35,10 +34,12 @@ def lambda_handler(event, context):
     upload_key = f"pp-{object_key}"
     img_download_path = f"/tmp/{object_key}"
 
+    curr_bucket, next_bucket = get_bucket_names()
+
     with open(img_download_path, "wb") as img_file:
-        get_file_from_bucket(source_bucket, key, img_file)
+        get_file_from_bucket(s3_client, source_bucket, key, img_file)
         process_image(img_download_path)
-        add_file_to_bucket(img_download_path, S3_DEST_BUCKET_NAME, upload_key)
+        add_file_to_bucket(s3_client, img_download_path, next_bucket, upload_key)
 
     # Delete the original file
-    remove_file_to_bucket(S3_BUCKET_NAME, key)
+    remove_file_from_bucket(s3_client, curr_bucket, key)
---
+++ Technical Competency Demonstrator/Lambda/test_image_preprocessor_lambda.py
@@ -0,0 +1,123 @@
+import unittest
+import boto3
+import tempfile
+
+from moto import mock_s3
+from unittest.mock import mock_open, patch
+from _pytest.monkeypatch import MonkeyPatch
+from image_preprocessor_lambda import (
+    add_file_to_bucket,
+    get_file_from_bucket,
+    remove_file_from_bucket,
+    process_image,
+    lambda_handler,
+)
+
+DEST_BUCKET_NAME = "DEST_BUCKET_NAME"
+BUCKET_NAME = "TEST_BUCKET"
+TEST_KEY = "TEST_KEY"
+
+
+@mock_s3
+class TestPreProcessingLambda(unittest.TestCase):
+    def setUp(self):
+        self.s3_client = boto3.client("s3")
+        self.s3_client.create_bucket(Bucket=BUCKET_NAME)
+        self.s3_client.create_bucket(Bucket=DEST_BUCKET_NAME)
+
+        self.test_data = b"col_1,col_2\n1,2\n3,4\n"
+
+        self.s3_client.put_object(
+            Body=self.test_data,
+            Bucket=BUCKET_NAME,
+            Key=TEST_KEY,
+        )
+
+        self.s3_event = {
+            "Records": [
+                {
+                    "s3": {
+                        "bucket": {"name": BUCKET_NAME},
+                        "object": {"key": TEST_KEY},
+                    }
+                }
+            ]
+        }
+
+        self.monkeypatch = MonkeyPatch()
+
+    def test_get_file_from_bucket(self):
+        mock = mock_open()
+        with patch("builtins.open", mock):
+
+            with open("test_file", "wb") as img_file:
+                get_file_from_bucket(self.s3_client, BUCKET_NAME, TEST_KEY, img_file)
+
+        # file was created to write tox
+        mock.assert_called_once_with("test_file", "wb")
+
+        # test_data was downloaded from bucket and written to
+        mock().write.assert_called_once_with(self.test_data)
+
+    def test_remove_file_from_bucket(self):
+        response = self.s3_client.get_object(Bucket=BUCKET_NAME, Key=TEST_KEY)
+        self.assertTrue(response["ResponseMetadata"]["HTTPStatusCode"] == 200)
+
+        # raises file from bucker
+        remove_file_from_bucket(self.s3_client, BUCKET_NAME, TEST_KEY)
+
+        # raises exception if accessing a key that does not exist (removed)
+        self.assertRaises(
+            self.s3_client.exceptions.NoSuchKey,
+            self.s3_client.get_object,
+            Bucket=BUCKET_NAME,
+            Key=TEST_KEY,
+        )
+
+    def test_add_file_to_bucket(self):
+
+        img_key = "test_key"
+
+        temp_file = tempfile.NamedTemporaryFile(dir="/tmp")
+        print(temp_file)
+
+        # raises exception if accessing a key that does not exist (doesn't exist yet)
+        self.assertRaises(
+            self.s3_client.exceptions.NoSuchKey,
+            self.s3_client.get_object,
+            Bucket=BUCKET_NAME,
+            Key=img_key,
+        )
+
+        add_file_to_bucket(self.s3_client, temp_file.name, BUCKET_NAME, img_key)
+        response = self.s3_client.get_object(Bucket=BUCKET_NAME, Key=img_key)
+
+        # response found the and returned the file
+        self.assertTrue(response["ResponseMetadata"]["HTTPStatusCode"] == 200)
+
+    @patch(
+        "image_preprocessor_lambda.get_bucket_names",
+        return_value=(BUCKET_NAME, DEST_BUCKET_NAME),
+    )
+    @patch("image_preprocessor_lambda.process_image")
+    @patch("image_preprocessor_lambda.get_file_from_bucket")
+    @patch("image_preprocessor_lambda.add_file_to_bucket")
+    @patch("image_preprocessor_lambda.remove_file_from_bucket")
+    def test_lambda_handler(
+        self,
+        get_bucket_names,
+        process_image,
+        get_file_from_bucket,
+        add_file_to_bucket,
+        remove_file_from_bucket,
+    ):
+        lambda_handler(event=self.s3_event, context={})
+        process_image.assert_called()
+        get_bucket_names.assert_called()
+        get_file_from_bucket.assert_called()
+        add_file_to_bucket.assert_called()
+        remove_file_from_bucket.assert_called()
+
+    def test_process_image(self):
+        process_image("")
+        self.assertTrue(True)
