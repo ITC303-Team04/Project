6cbccafe9d8c43af74b841ba7d96425a66ab254c
Bryce Sandilands <bryce.sandilands@gmail.com>
2022-06-05 19:39:01 -0700

    Ported image pre-processor lambda over using docker

---
+++ Technical Competency Demonstrator/Docker/Lambda_Preprocessor/app.py
@@ -0,0 +1,45 @@
+import boto3
+import uuid
+
+
+def get_bucket_names(
+    source="sagemaker-training-csu303", destination="preprocessed-images-csu303"
+):
+    return source, destination
+
+
+def get_file_from_bucket(client, source_bucket, key, img_file):
+    client.download_fileobj(source_bucket, key, img_file)
+
+
+def process_image(img_path):
+    pass
+
+
+def add_file_to_bucket(client, img_path, dest_bucket_name, upload_key):
+    client.upload_file(img_path, dest_bucket_name, upload_key)
+
+
+def remove_file_from_bucket(client, source_bucket, key):
+    client.delete_object(Bucket=source_bucket, Key=key)
+
+
+def lambda_handler(event, context):
+    s3_client = boto3.client("s3")
+    # getting bucket and object key from event object
+    source_bucket = event["Records"][0]["s3"]["bucket"]["name"]
+    key = event["Records"][0]["s3"]["object"]["key"]
+
+    object_key = f"{uuid.uuid4()}-{key}"
+    upload_key = f"pp-{object_key}"
+    img_download_path = f"/tmp/{object_key}"
+
+    curr_bucket, next_bucket = get_bucket_names()
+
+    with open(img_download_path, "wb") as img_file:
+        get_file_from_bucket(s3_client, source_bucket, key, img_file)
+        process_image(img_download_path)
+        add_file_to_bucket(s3_client, img_download_path, next_bucket, upload_key)
+
+    # Delete the original file
+    remove_file_from_bucket(s3_client, curr_bucket, key)
