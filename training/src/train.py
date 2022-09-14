edc7e67c9225bc627dfcded0b5901d8cc4d8ec40
Bryce Sandilands <bryce.sandilands@gmail.com>
2022-09-13 20:42:33 -0700

    Checkpoint before DCS AWS transfer

--- training/src/train.py
+++ training/src/train.py
@@ -1,17 +1,16 @@
 import argparse
 import os
+import tarfile
+import boto3
 import numpy as np
 import tensorflow as tf
 import cv2 as cv
-
-from keras.layers import Input, Conv2D, Activation, BatchNormalization, Conv2DTranspose, Concatenate
-from keras.layers.pooling import MaxPooling2D
-from keras.models import Model, load_model
-from keras import backend as K
-
 import pandas as pd
 
-# get folder structure from corey and add to docs
+from tensorflow.keras.layers import Input, Conv2D, Activation, BatchNormalization, Conv2DTranspose, Concatenate, MaxPooling2D
+from tensorflow.keras.models import Model, load_model, save_model
+from tensorflow.keras import backend as K
+    
 # //==================== create df ====================//
 def createDataFrame(train_dir):
     sat_image = []
@@ -38,19 +37,21 @@ def modify_mask(mask):
     return t_mask
 
 def map_function(img, mask, img_shape):
+    img_shape_tuple = tuple(img_shape[:2])
+    
     img = cv.imread(img.decode())
     mask = cv.imread(mask.decode(), cv.IMREAD_GRAYSCALE)
-    img = cv.resize(img, img_shape)
-    mask = modify_mask(cv.resize(mask, img_shape))
+    img = cv.resize(img, img_shape_tuple)
+    mask = modify_mask(cv.resize(mask, img_shape_tuple))
     img = img/255.0
 
     return img.astype(np.float64), mask.astype(np.float64)
 
-def create_dataset(data, batch_size):
+def create_dataset(data, batch_size, img_shape):
     dataset = tf.data.Dataset.from_tensor_slices((data['Image'], data['Mask']))
     dataset = dataset.shuffle(1000)
     dataset = dataset.map(lambda img, mask : tf.numpy_function(
-                    map_function, [img, mask], [tf.float64, tf.float64]),
+                    map_function, [img, mask, img_shape], [tf.float64, tf.float64]),
                     num_parallel_calls = tf.data.experimental.AUTOTUNE).batch(batch_size)
 
     dataset = dataset.prefetch(buffer_size = tf.data.experimental.AUTOTUNE)
@@ -142,43 +143,78 @@ def unet(learning_rate, input_shape, num_classes):
   x = Conv2D(num_classes, (3, 3), activation='sigmoid', padding='same')(x)
  
   model = Model(inputs=inp, outputs=x)
-  adam = tf.keras.optimizers.Adam(lr=0.0001)
+  adam = tf.keras.optimizers.Adam(lr=learning_rate)
   model.compile(optimizer=adam, loss=dice_coef_loss, metrics=[dice_coef])
   return model
 
-def retrain_existing_model(model_path):
-    return load_model(model_path, compile=True, custom_objects={"dice_coef_loss": dice_coef_loss, "dice_coef": dice_coef})
+# //==================== AWS functions ====================//
+def log(msg, output=''):
+    return f'{"="*20} [{msg.upper}] {"="*20} \n{output}'
 
-
-def parseArgs():
+def parse_args():
     parser = argparse.ArgumentParser()
 
-    # hyperparameters sent by the client are passed as command-line arguments to the script.
     parser.add_argument('--epochs', type=int, default=1)
     parser.add_argument('--learning-rate', type=float, default=0.0001)
-    parser.add_argument('--img-shape', type=tuple, default=(512, 512))
+    parser.add_argument('--img-shape', type=tuple, default=(512, 512, 3))
     parser.add_argument('--batch-size', type=int, default=4)
+    parser.add_argument('--num-classes', type=int, default=1)
     parser.add_argument('--retrain-model', type=bool, default=False)
+    parser.add_argument('--bucket', type=str)
 
-    # data, model, and output directories. These are required.
-    # =====from sagemaker
-    parser.add_argument("--model-dir", type=str)
-    parser.add_argument("--output-dir", type=str, default=os.environ["SM_OUTPUT_DIR"])
+    parser.add_argument("--model-dir", type=str, default=os.environ["SM_MODEL_DIR"])
     parser.add_argument("--train", type=str, default=os.environ["SM_CHANNEL_TRAIN"])
-    parser.add_argument("--test", type=str, default=os.environ["SM_CHANNEL_TEST"])
 
-    # ... load from args.train and args.test, train a model, write model to args.model_dir.
     args, _ = parser.parse_known_args()
     return args
 
+def retrain_existing_model(file, bucket):
+    log('Not yet implemented')
+    exit()
+#     path = f'opt/ml/retrain/{file}'
+#     download_from_s3(file, bucket, path)
+    
+#     with tarfile.open(path) as model_tar:
+#         for entry in model_tar:
+#             #
+            
+#     model = load_model(model_path, compile=True, custom_objects={"dice_coef_loss": dice_coef_loss, "dice_coef": dice_coef}) 
+#     return model
+
+def compress_model():
+    with tarfile.open(os.path.join(args.model_dir, 'model.tar.gz'), "w:gz") as tar:
+        tar.add(os.path.join(args.model_dir, 'model.h5'), arcname='model.h5')
+        tar.add(os.path.join(args.model_dir, 'model.json'), arcname='model.json')
+    return tar
+
+def upload_to_s3(path, file, bucket_name):
+    s3 = boto3.Session().resource('s3')
+    bucket = s3.Bucket(bucket_name[5:])
+    obj = bucket.Object(os.path.join('model', file))
+
+    try:
+        response = obj.upload_file(os.path.join(path, file))
+        
+    except Exception as e:
+        log('s3 upload failed', e)
+        
+def download_from_s3(file_name, bucket_name, path):
+    log('Not yet implemented')
+    exit()
+#     s3 = boto3.Session().resource('s3')
+#     bucket = s3.Bucket(bucket_name[5:])
+#     key = f'retrain/{file_name}'
+#     for file in bucket.objects.all():
+#         if file.key == key:
+#             bucket.download_file(file.key, file.key)
 
 
 if __name__ =='__main__':
-    args = parseArgs()
-    print(args)
+
+    args = parse_args()
 
     if args.retrain_model:
-        model = retrain_existing_model(args.model_dir)
+        model = retrain_existing_model('model.tar.gz', args.bucket)
     else:
         model = unet(
             learning_rate=args.learning_rate,
@@ -187,11 +223,23 @@ if __name__ =='__main__':
         )
     
     callbacks = [
-        tf.keras.callbacks.ModelCheckpoint(args.output_dir, monitor="loss", save_best_only=True),
+        tf.keras.callbacks.ModelCheckpoint(
+            os.path.join(args.model_dir, 'model.h5'),
+            monitor="loss",
+            save_best_only=True,
+            save_weights_only=True,
+        ),
         tf.keras.callbacks.EarlyStopping(monitor='loss', mode='min', verbose=1, patience=30),
     ]
     
     df = createDataFrame(args.train)   
-    train_dataset = create_dataset(df, args.batch_size)
+    train_dataset = create_dataset(df, args.batch_size, args.img_shape)
 
-    model_history = model.fit(train_dataset, callbacks=callbacks, epochs=args.epochs)
+    history = model.fit(train_dataset, callbacks=callbacks, epochs=args.epochs)
+
+    model_json = model.to_json()
+    with open(os.path.join(args.model_dir, 'model.json'), "w") as json_file:
+        json_file.write(model_json)
+    
+    compressed_model = compress_model()
+    upload_to_s3(args.model_dir, 'model.tar.gz', args.bucket)
